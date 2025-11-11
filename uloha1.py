be49631b8d30ebca9272a4dedf012d7fb9406839
polonecmichal <polonecmichal36@gmail.com>
2025-11-11 17:20:08 +0100

    commit

---
+++ uloha1.py
@@ -0,0 +1,17 @@
+# ============================================================================
+# PRÍKLAD 1: Základné operácie so stringmi
+# ============================================================================
+"""
+Vytvorte string s vašim menom a priezviskom.
+Vypíšte ho veľkými písmenami, malými písmenami a s prvým veľkým písmenom.
+"""
+
+# Vaše riešenie:
+meno = "Michal Polonec"
+
+print(meno.upper())
+print(meno.lower())
+print(meno.capitalize())
+
+
+       
